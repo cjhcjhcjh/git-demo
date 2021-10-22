fa122ca1223214670e687a4b75df67b912b03fba
chenJh <1023615551@qq.com>
2021-10-22 15:17:26 +0800

    hot-fix commit

--- src/main/java/com/cjh/git/gitdemo/GitDemoApplication.java
+++ src/main/java/com/cjh/git/gitdemo/GitDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("hot-fix");
         System.out.println("aa");
         SpringApplication.run(GitDemoApplication.class, args);
     }
